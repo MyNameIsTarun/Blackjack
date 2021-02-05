aee8617f824ca74fdc26401b0e8d790f18bf9748
iamtarun <tarunkanademhs@gmail.com>
2021-02-05 10:16:09 +0530

    Added Card and Deck class

---
+++ Card.java
@@ -0,0 +1,51 @@
+package Blackjack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Tarun
+ */
+public class Card {
+    //Map(Dictionary) to hold rank and value pairs of cards
+    private static final Map<String,Integer> values;
+    
+    static{
+        values = new HashMap();
+        values.put("Two",2);values.put("Three",3);
+        values.put("Four",4);values.put("Five",5);
+        values.put("Six",6);values.put("Seven",7);
+        values.put("Eight",8);values.put("Nine",9);
+        values.put("Ten",10);values.put("Jack",11);
+        values.put("Queen",12);values.put("King",13);
+        values.put("Ace",14);
+    }
+    
+    private final String suit;
+    private final String rank;
+    private final int value;
+    
+    public Card(String suit,String rank){
+        this.suit = suit;
+        this.rank = rank;
+        this.value = values.get(rank);
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }    
+    
+    public int getValue() {
+        return value;
+    }
+    
+    @Override
+    public String toString(){
+        return rank+" of "+suit;
+    }
+}
---
+++ Deck.java
@@ -0,0 +1,43 @@
+package Blackjack;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Tarun
+ */
+public class Deck {
+    
+    //arrays for suits and ranks of cards
+    //this will help to create deck of Card objects
+    private final String []suits = {"Hearts","Diamonds","Spades","Clubs"};
+    private final String []ranks = {"Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
+    
+    //this will hold the 52 card objects
+    private List<Card> deck = new ArrayList<>();
+    
+    public Deck(){
+        //creating deck by adding card objects to deck list
+        for(String suit : suits){
+            for(String rank : ranks){
+                deck.add(new Card(suit,rank));
+            }
+        }
+    }
+    
+    public List<Card> getDeck(){
+        return deck;
+    }
+    
+    //this method shuffles the deck at inplace
+    public void shuffle(){
+        Collections.shuffle(deck);
+    }
+    
+    //this will remove one card from the end of the deck
+    public Card deal_one(){
+        return deck.remove(deck.size()-1);
+    }
+}
